82fa25dcf6a8863ab4df9645eab8569e34140de9
dima9891 <rezvykh@mail.ru>
2021-06-04 07:03:22 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,88 @@
+// Upload picture from input or parse json
+document.querySelector('#upload-file-from-url').addEventListener('click', function() {
+    if (document.querySelector('#input-url').value === '') {
+        return false;
+    } else {
+        const inputText = document.querySelector('#input-url').value
+
+        if (inputText.includes('http')) {
+            if (inputText.includes('json')) {
+                fetchAndParseJson(inputText)
+                document.querySelector('#input-url').value = ''
+            } if (inputText.includes('jpg') | inputText.includes('jpeg')) {
+                addToGallery(inputText)
+                document.querySelector('#input-url').value = ''
+            } else {return false;}
+        }
+            
+    }
+})
+
+// Delete image from gallery
+document.addEventListener('click', function(e){
+    if(e.target && e.target.matches('.images-gallery__delete-btn')){
+         e.target.parentNode.remove()
+    }
+});
+
+// This functions adds image to the end of the gallery
+function addToGallery(url) {
+    let imgWrapper = document.createElement("div")
+    imgWrapper.classList.add('images-gallery__image-wrapper')
+    
+    let image = document.createElement("img")
+    image.classList.add('images-gallery__image')
+    image.src = url
+    
+    let delBtn = document.createElement("div")
+    delBtn.classList.add('images-gallery__delete-btn')
+
+    imgWrapper.appendChild(image)
+    imgWrapper.appendChild(delBtn)
+    
+    document.querySelector('.images-gallery').appendChild(imgWrapper)
+}
+
+// This functions fetches and parse json file and adds all images grom file to the gallery
+function fetchAndParseJson(url) {
+    fetch(url)
+    .then(response => response.json())
+    .then(function(data) {
+        for (let key in data.galleryImages) {
+            
+            let imageUrl = data.galleryImages[key].url
+            addToGallery(imageUrl)
+            } 
+    })
+}
+
+// Drap and drop image to the gallery
+function dropHandler(ev) {
+
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+        for (let i = 0; i < ev.dataTransfer.items.length; i++) {
+            if (ev.dataTransfer.items[i].kind === 'file' && ev.dataTransfer.items[i].type === 'image/jpeg') {
+                console.log(ev.dataTransfer.items[i].type)
+                let file = ev.dataTransfer.items[i].getAsFile();
+                console.log('... file[' + i + '].name = ' + file.name)
+                previewFile(file)
+            }
+        }
+    } else {
+        return false
+    }
+}
+
+function dragOverHandler(ev) {
+    ev.preventDefault();
+}
+
+function previewFile(file) {
+    let reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onloadend = function () {
+        addToGallery(reader.result)
+    }
+}
