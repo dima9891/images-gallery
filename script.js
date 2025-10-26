59d5314749ac5ab6b3d01ab2e49e8fa48eb878ba
Дмитрий Резвых <rezvykh22@gmail.com>
2025-10-26 13:08:28 +0300

    add images from json update

--- script.js
+++ script.js
@@ -1,86 +1,87 @@
-// Upload picture from input or parse json
-document.querySelector('#upload-file-from-url').addEventListener('click', function() {
-    if (document.querySelector('#input-url').value === '') {
-        return false;
-    } else {
-        const inputText = document.querySelector('#input-url').value
+document
+  .querySelector("#upload-file-from-url")
+  .addEventListener("click", async function () {
+    const input = document.querySelector("#input-url");
 
-        if (inputText.includes('http')) {
-            if (inputText.includes('json')) {
-                fetchAndParseJson(inputText)
-                document.querySelector('#input-url').value = ''
-            } if (inputText.includes('jpg') | inputText.includes('jpeg')) {
-                addToGallery(inputText)
-                document.querySelector('#input-url').value = ''
-            } else {return false;}
-        }
-            
+    if (input.value === "") {
+      return false;
     }
-})
 
-// Delete image from gallery
-document.addEventListener('click', function(e){
-    if(e.target && e.target.matches('.images-gallery__delete-btn')){
-         e.target.parentNode.remove()
+    let url;
+    try {
+      url = new URL(input.value);
+    } catch (e) {
+      console.error("Url is incorrect " + e.message);
+      input.value = "";
+      return false;
     }
+
+    if (!url.toString().includes("json")) {
+      return false;
+    }
+
+    const data = await fetch(url.toString());
+    if (!data.ok) {
+      throw new Error(`HTTP error! status: ${data.status}`);
+    }
+
+    const json = await data.json();
+
+    json.galleryImages.forEach((img) => {
+      addToGallery(img.url);
+    });
+  });
+
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.matches(".images-gallery__delete-btn")) {
+    e.target.parentNode.remove();
+  }
 });
 
-// This functions adds image to the end of the gallery
-function addToGallery(url) {
-    let imgWrapper = document.createElement("div")
-    imgWrapper.classList.add('images-gallery__image-wrapper')
-    
-    let image = document.createElement("img")
-    image.classList.add('images-gallery__image')
-    image.src = url
-    
-    let delBtn = document.createElement("div")
-    delBtn.classList.add('images-gallery__delete-btn')
-
-    imgWrapper.appendChild(image)
-    imgWrapper.appendChild(delBtn)
-    
-    document.querySelector('.images-gallery').appendChild(imgWrapper)
+function addToGallery(
+  url,
+  gallery = document.querySelector(".images-gallery")
+) {
+  if(!gallery) {
+    return
+  }
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `
+        <div class="images-gallery__image-wrapper">
+            <img class="images-gallery__image" src="${url}">
+            <div class="images-gallery__delete-btn"></div>
+        </div>
+    `
+  );
 }
 
-// This functions fetches and parse json file and adds all images grom file to the gallery
-function fetchAndParseJson(url) {
-    fetch(url)
-    .then(response => response.json())
-    .then(function(data) {
-        for (let key in data.galleryImages) {
-            
-            let imageUrl = data.galleryImages[key].url
-            addToGallery(imageUrl)
-            } 
-    })
-}
-
-// Drap and drop image to the gallery
 function dropHandler(ev) {
+  ev.preventDefault();
 
-    ev.preventDefault();
-
-    if (ev.dataTransfer.items) {
-        for (let i = 0; i < ev.dataTransfer.items.length; i++) {
-            if (ev.dataTransfer.items[i].kind === 'file' && ev.dataTransfer.items[i].type === 'image/jpeg') {
-                let file = ev.dataTransfer.items[i].getAsFile();
-                addFileToGallery(file)
-            }
-        }
-    } else {
-        return false
+  if (ev.dataTransfer.items) {
+    for (let i = 0; i < ev.dataTransfer.items.length; i++) {
+      if (
+        ev.dataTransfer.items[i].kind === "file" &&
+        ev.dataTransfer.items[i].type === "image/jpeg"
+      ) {
+        let file = ev.dataTransfer.items[i].getAsFile();
+        addFileToGallery(file);
+      }
     }
+  } else {
+    return false;
+  }
 }
 
 function dragOverHandler(ev) {
-    ev.preventDefault();
+  ev.preventDefault();
 }
 
 function addFileToGallery(file) {
-    let reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onloadend = function () {
-        addToGallery(reader.result)
-    }
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = function () {
+    addToGallery(reader.result);
+  };
 }
